5f4c358879e325c8632d3a86c87d595830199b7f
Stefan Jacobs <stefan@zenaptix.com>
2019-04-09 19:45:54 +0200

    Implement implicits needed by OfferRoutes. Bind on interface 0.0.0.0 - docker requirement.

--- src/main/scala/com/wp/QuickstartServer.scala
+++ src/main/scala/com/wp/QuickstartServer.scala
@@ -3,25 +3,17 @@ package com.wp
 import scala.concurrent.{ Await, ExecutionContext, Future }
 import scala.concurrent.duration.Duration
 import scala.util.{ Failure, Success }
-
-import akka.actor.{ ActorRef, ActorSystem }
+import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.server.Route
 import akka.stream.ActorMaterializer
 
-object QuickstartServer extends App with UserRoutes {
+object QuickstartServer extends App with OfferRoutes {
 
-  // set up ActorSystem and other dependencies here
-  implicit val system: ActorSystem = ActorSystem("helloAkkaHttpServer")
+  implicit val system: ActorSystem = ActorSystem("offerServer")
+  implicit val ec: ExecutionContext = system.dispatcher
   implicit val materializer: ActorMaterializer = ActorMaterializer()
-  implicit val executionContext: ExecutionContext = system.dispatcher
 
-  val userRegistryActor: ActorRef = system.actorOf(UserRegistryActor.props, "userRegistryActor")
-
-  // from the UserRoutes trait
-  lazy val routes: Route = userRoutes
-
-  val serverBinding: Future[Http.ServerBinding] = Http().bindAndHandle(routes, "localhost", 8080)
+  val serverBinding: Future[Http.ServerBinding] = Http().bindAndHandle(offerRoutes, "0.0.0.0", 8080)
 
   serverBinding.onComplete {
     case Success(bound) =>
@@ -33,4 +25,5 @@ object QuickstartServer extends App with UserRoutes {
   }
 
   Await.result(system.whenTerminated, Duration.Inf)
+
 }
