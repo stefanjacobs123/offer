6cabd7c5cc8b6e04575dcbf51302a826bb6afec7
Stefan Jacobs <stefan@zenaptix.com>
2019-04-09 19:44:54 +0200

    Add JSON support for new case classes & implicit converters for LocalDateTime and UUID.

--- src/main/scala/com/wp/JsonSupport.scala
+++ src/main/scala/com/wp/JsonSupport.scala
@@ -1,16 +1,34 @@
 package com.wp
 
-import com.wp.UserRegistryActor.ActionPerformed
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter.ISO_LOCAL_DATE_TIME
+import java.util.UUID
 
+import com.wp.OfferActor.{ CancelOffer, OfferCreated }
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import spray.json.DefaultJsonProtocol
+import spray.json.{ DefaultJsonProtocol, DeserializationException, JsString, JsValue, RootJsonFormat }
 
 trait JsonSupport extends SprayJsonSupport {
-  // import the default encoders for primitive types (Int, String, Lists etc)
   import DefaultJsonProtocol._
 
-  implicit val userJsonFormat = jsonFormat3(User)
-  implicit val usersJsonFormat = jsonFormat1(Users)
+  implicit object LocalDateTimeJsonFormat extends RootJsonFormat[LocalDateTime] {
+    override def write(obj: LocalDateTime): JsValue = JsString(obj.format(ISO_LOCAL_DATE_TIME))
+    override def read(json: JsValue): LocalDateTime = json match {
+      case JsString(s) => LocalDateTime.parse(s, ISO_LOCAL_DATE_TIME)
+      case _ => throw DeserializationException("No string found")
+    }
+  }
 
-  implicit val actionPerformedJsonFormat = jsonFormat1(ActionPerformed)
+  implicit object UUIDJsonFormat extends RootJsonFormat[UUID] {
+    override def write(obj: UUID): JsValue = JsString(obj.toString)
+    override def read(json: JsValue): UUID = json match {
+      case JsString(s) => UUID.fromString(s)
+      case _ => throw DeserializationException("Error info you want here ...")
+    }
+  }
+
+  implicit val offerJsonFormat = jsonFormat4(Offer)
+  implicit val offerCreatedJsonFormat = jsonFormat1(OfferCreated)
+  implicit val cancelOfferJsonFormat = jsonFormat1(CancelOffer)
+
 }
